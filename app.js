4191675c0a5c6cb51e3fc72b9b391afd3294154a
prachiii26 <prachiupadhyay85@gmail.com>
2026-06-17 02:38:41 +0530

    Fix session config

--- app.js
+++ app.js
@@ -53,13 +53,13 @@ const store = MongoStore.create({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
    console.log("ERROR in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
    store,
-   secret: "process.env.SECRET",
+   secret: process.env.SECRET,
    resave: false,
    saveUninitialized: true,
    cookie: {
